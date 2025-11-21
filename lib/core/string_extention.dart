fc3a220e7f13c22e32e928f8527b7a847f65b536
Farhad <farhad@toomba.nl>
2025-11-21 16:13:44 +0100

    Fixed menu key

--- lib/core/string_extention.dart
+++ lib/core/string_extention.dart
@@ -13,6 +13,11 @@ extension StringExtension on String {
     // Remove All Spcial Characters for all Languages except '-'
     res = res.replaceAll(RegExp(r'[^\p{L}\p{N}\-]+', unicode: true), '');
 
+    // Fixed first number issue
+    if (res.isNotEmpty && RegExp(r'^[0-9]').hasMatch(res)) {
+      res = 'n$res';
+    }
+
     return res;
   }
 }
